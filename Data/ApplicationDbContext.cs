71dc1bb70de56ff40375ca48cba17614aab24a7f
Ivan Dupcek <ivan@qualityhub.co.nz>
2021-11-09 07:30:28 +1300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TheBlogProject.Models;
+
+namespace TheBlogProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BlogUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Enums/ModerationType.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace TheBlogProject.Enums
+{
+    public enum ModerationType
+    {
+        [Description("Political Propaganda")]
+        Political,
+        [Description("Offencive Language")]
+        Language,
+        [Description("Drug References")]
+        Drugs,
+        [Description("Threatening Speech")]
+        Threatening,
+        [Description("Sexual Content")]
+        Sexual,
+        [Description("Hate Speech")]
+        HateSpeech,
+        [Description("Targeted Shaming")]
+        Shaming
+    }
+}
